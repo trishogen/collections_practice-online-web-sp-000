382832b49548c29c566952fccb114ab654e58a93
Trish Ogen <patriciaogen@gmail.com>
2019-05-19 17:53:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,3 +30,10 @@ end
 def sum_array(array)
   array.inject{|sum, n| sum + n}
 end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element + "s"
+    end
+end
