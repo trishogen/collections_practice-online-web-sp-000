7b05e72ef81c2bb01243e61ee04d0d93b902063f
Trish Ogen <patriciaogen@gmail.com>
2019-05-19 17:31:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,10 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a, b|
-    a == 1 <=> b == 2
-    end
-  end
+  array.sort {|a, b| a == 1 <=> b == 2}
 end
 
 def reverse_array(array)
